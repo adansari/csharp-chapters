7513dc35e795ffe44e98d04af9ee42de65df6799
Adil Ansari <mailbox.adil@gmail.com>
2023-02-05 16:20:02 +0530

    Add Chapter1_PeriocidTimer

---
+++ CSharpChapters/Chapter1_PeriocidTimer/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static int secs = 0, mins = 0;
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("secs: 00");
+        Console.WriteLine("mins: 00");
+
+        var secondTicks = SecondTicker();
+        var minuteTicks = MinuteTicker();
+        await Task.WhenAll(secondTicks, minuteTicks);
+    }
+
+    static async Task SecondTicker()
+    {
+        PeriodicTimer secondTimer = new PeriodicTimer(new TimeSpan(0, 0, 1));
+
+        while (await secondTimer.WaitForNextTickAsync())
+        {
+            secs++;
+            Console.SetCursorPosition(0, 0);
+            Console.Write($"secs: {secs.ToString("00")}");
+        }
+    }
+
+    static async Task MinuteTicker()
+    {
+        PeriodicTimer minuteTimer = new PeriodicTimer(new TimeSpan(0, 1, 0));
+
+        while (await minuteTimer.WaitForNextTickAsync())
+        {
+            mins++;
+            secs = 0;
+            Console.SetCursorPosition(0, 1);
+            Console.Write($"mins: {mins.ToString("00")}");
+        }
+    }
+}
